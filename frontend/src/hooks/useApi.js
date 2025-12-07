710b5981af390930950dcb73054833fb4359417e
Pecorari <pecorari.clemente@gmail.com>
2025-12-07 20:38:06 -0300

    alterando useApi

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/json;charset=utf-8',
   }
 });
 
